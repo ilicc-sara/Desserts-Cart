c4350cb9f04c2f22c7aeaa16c0b28e09f2d83f53
Sara <sarailic431@gmail.com>
2025-05-01 12:32:16 +0200

    rendering items

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Item from "./Item.jsx";
 
 const info = [
   {
@@ -7,63 +8,82 @@ const info = [
     name: "Waffle with Berries",
     price: 6.5,
     image: "./image-waffle-desktop.jpg",
+    id: 1,
   },
   {
     type: "Crème Brûlée",
     name: "Vanilla Bean Crème Brûlée",
     price: 7.0,
     image: "./image-creme-brulee-desktop.jpg",
+    id: 2,
   },
   {
     type: "Macaron",
     name: "Macaron Mix of Five",
     price: 8.0,
     image: "./image-macaron-desktop.jpg",
+    id: 3,
   },
   {
     type: "Tiramisu",
     name: "Classic Tiramisu",
     price: 5.5,
     image: "./image-tiramisu-desktop.jpg",
+    id: 4,
   },
   {
     type: "Baklava",
     name: "Pistachio Baklava",
     price: 4.0,
     image: "./image-baklava-desktop.jpg",
+    id: 5,
   },
   {
     type: "Pie",
     name: "Lemon Meringue Pie",
     price: 5.0,
     image: "./image-meringue-desktop.jpg",
+    id: 6,
   },
   {
     type: "Cake",
     name: "Red Velvet Cake",
     price: 4.5,
     image: "./image-cake-desktop.jpg",
+    id: 7,
   },
   {
     type: "Brownie",
     name: "Salted Caramel Brownie",
     price: 4.5,
     image: "./image-brownie-desktop.jpg",
+    id: 8,
   },
   {
     type: "Panna Cotta",
     name: "Vanilla Panna Cotta",
     price: 6.5,
     image: "./image-panna-cotta-desktop.jpg",
+    id: 9,
   },
 ];
 
 function App() {
+  const [items, setItems] = useState(info);
   return (
     <>
       <div class="desserts-list">
         <h1>Desserts</h1>
-        <article class="desert-item"></article>
+        {items.map((item) => (
+          <Item
+            key={item.id}
+            type={item.type}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+            id={item.id}
+          />
+        ))}
       </div>
       <div class="cart"></div>
     </>
--- src/Item.jsx
+++ src/Item.jsx
@@ -1,1 +1,26 @@
 import React, { useState } from "react";
+
+function Item(props) {
+  const { type, name, price, image } = props;
+  return (
+    <article class="desert-item">
+      <div class="dessert-pic-cont">
+        <img class="dessert-pic" src={image} />
+        <div class="cart-button">
+          <i class="bx bx-cart-add">
+            {" "}
+            <span>Add to Cart</span>
+          </i>
+        </div>
+      </div>
+
+      <div class="info-cont">
+        <p class="type">{type}</p>
+        <h4 class="name">{name}</h4>
+        <span class="price">$ {price.toFixed(2)}</span>
+      </div>
+    </article>
+  );
+}
+
+export default Item;
